192a555500f25cd12285a168381b83ce0bf209ca
voyager2005 <76808676+voyager2005@users.noreply.github.com>
2021-02-04 00:40:22 +0530

    update: print statement
    self promotion 🤣🤣🤣

--- aeroplane_seat_reservation_system.py
+++ aeroplane_seat_reservation_system.py
@@ -41,7 +41,8 @@ def display_welcome():
     print(" *   *   *   * * * *   *       *        *         *  *  *  *   * * * * ")
     print(" * *   * *   *         *        *        *       *   *     *   *       ")
     print(" *       *   * * * *   * * * *   * * *    * * * *    *     *   * * * * ")
-
+    print(f"{bcolors.BOLD}{bcolors.HEADER}{bcolors.UNDERLINE}Welcome to https://github.com.voyager2005 Travels, "
+          f"We would like to take some information: {bcolors.ENDC}{bcolors.ENDC}{bcolors.ENDC}")
 
 # accepting the users name
 def accept_name():
